88726907699655d581dd0620ee4c0bad725299b3
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2023-10-07 05:58:28 -0500

    ramang

--- src/main/java/org/example/nataliaGonzales/NataliaGonzales.java
+++ src/main/java/org/example/nataliaGonzales/NataliaGonzales.java
@@ -1,2 +1,5 @@
-package org.example.nataliaGonzales;public class NataliaGonzales {
+package org.example.nataliaGonzales;
+
+public class NataliaGonzales {
+    //no hice nada, solo observe y repase los conceptos y el trabajo de mis compañeros
 }
